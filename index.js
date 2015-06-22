981179e06afe890aa3f7964e442fb25c99b5f3ab
SEKE <sebastian.kerckhof@barco.com>
2015-06-22 13:49:00 +0200

    Convert colons on windows.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var echo = function(name) {
 }
 
 var normalize = !win32 ? echo : function(name) {
-  return name.replace(/\\/g, '/')
+  return name.replace(/\\/g, '/').replace(/:/g, '_');
 }
 
 var statAll = function(fs, stat, cwd, ignore, entries) {
@@ -202,6 +202,8 @@ exports.extract = function(cwd, opts) {
 
   extract.on('entry', function(header, stream, next) {
     header = map(header) || header
+
+    header.name = normalize(header.name);
     var name = path.join(cwd, path.join('/', header.name))
 
     if (ignore(name)) {
