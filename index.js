a1a1fb8ff25c80529b13b8a6b57419803153137d
Jesús Leganés Combarro <piranna@gmail.com>
2016-05-23 00:10:33 +0200

    Allow to create a file when filesystem don't support hard links (#44)
    * Allow to create a file when filesystem don't support hard links
    
    * Changed option name from `allowCreateFilesForHardLinks` to `hardlinkAsFilesFallback`

--- index.js
+++ index.js
@@ -247,7 +247,16 @@ exports.extract = function (cwd, opts) {
     var onlink = function () {
       if (win32) return next() // skip links on win for now before it can be tested
       xfs.unlink(name, function () {
-        xfs.link(path.resolve(cwd, header.linkname), name, stat)
+        var srcpath = path.resolve(cwd, header.linkname)
+
+        xfs.link(srcpath, name, function (err) {
+          if (err && err.code === 'EPERM' && opts.hardlinkAsFilesFallback) {
+            stream = xfs.createReadStream(srcpath)
+            return onfile()
+          }
+
+          stat(err)
+        })
       })
     }
 
