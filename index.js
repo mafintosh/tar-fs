06672828e6fa29ac8551b1b6f36c852a9a3c58a2
Mathias Buus <mathiasbuus@gmail.com>
2018-04-30 13:20:56 +0200

    force hardlink targets to be in the tar

--- index.js
+++ index.js
@@ -261,7 +261,7 @@ exports.extract = function (cwd, opts) {
     var onlink = function () {
       if (win32) return next() // skip links on win for now before it can be tested
       xfs.unlink(name, function () {
-        var srcpath = path.resolve(cwd, header.linkname)
+        var srcpath = path.join(cwd, path.join('/', header.linkname))
 
         xfs.link(srcpath, name, function (err) {
           if (err && err.code === 'EPERM' && opts.hardlinkAsFilesFallback) {
