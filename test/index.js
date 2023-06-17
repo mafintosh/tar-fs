8ab9721d74a4b2d5fe85f45159d9d4260cf9c992
Mathias Buus <mathiasbuus@gmail.com>
2023-06-17 21:58:51 +0200

    upgrade to latest deps

--- test/index.js
+++ test/index.js
@@ -1,32 +1,32 @@
-var test = require('tape')
-var rimraf = require('rimraf')
-var tar = require('../index')
-var tarStream = require('tar-stream')
-var path = require('path')
-var fs = require('fs')
-var os = require('os')
+const test = require('tape')
+const rimraf = require('rimraf')
+const tar = require('../index')
+const tarStream = require('tar-stream')
+const path = require('path')
+const fs = require('fs')
+const os = require('os')
 
-var win32 = os.platform() === 'win32'
+const win32 = os.platform() === 'win32'
 
-var mtime = function (st) {
+const mtime = function (st) {
   return Math.floor(st.mtime.getTime() / 1000)
 }
 
 test('copy a -> copy/a', function (t) {
   t.plan(5)
 
-  var a = path.join(__dirname, 'fixtures', 'a')
-  var b = path.join(__dirname, 'fixtures', 'copy', 'a')
+  const a = path.join(__dirname, 'fixtures', 'a')
+  const b = path.join(__dirname, 'fixtures', 'copy', 'a')
 
   rimraf.sync(b)
   tar.pack(a)
     .pipe(tar.extract(b))
     .on('finish', function () {
-      var files = fs.readdirSync(b)
+      const files = fs.readdirSync(b)
       t.same(files.length, 1)
       t.same(files[0], 'hello.txt')
-      var fileB = path.join(b, files[0])
-      var fileA = path.join(a, files[0])
+      const fileB = path.join(b, files[0])
+      const fileA = path.join(a, files[0])
       t.same(fs.readFileSync(fileB, 'utf-8'), fs.readFileSync(fileA, 'utf-8'))
       t.same(fs.statSync(fileB).mode, fs.statSync(fileA).mode)
       t.same(mtime(fs.statSync(fileB)), mtime(fs.statSync(fileA)))
@@ -36,23 +36,23 @@ test('copy a -> copy/a', function (t) {
 test('copy b -> copy/b', function (t) {
   t.plan(8)
 
-  var a = path.join(__dirname, 'fixtures', 'b')
-  var b = path.join(__dirname, 'fixtures', 'copy', 'b')
+  const a = path.join(__dirname, 'fixtures', 'b')
+  const b = path.join(__dirname, 'fixtures', 'copy', 'b')
 
   rimraf.sync(b)
   tar.pack(a)
     .pipe(tar.extract(b))
     .on('finish', function () {
-      var files = fs.readdirSync(b)
+      const files = fs.readdirSync(b)
       t.same(files.length, 1)
       t.same(files[0], 'a')
-      var dirB = path.join(b, files[0])
-      var dirA = path.join(a, files[0])
+      const dirB = path.join(b, files[0])
+      const dirA = path.join(a, files[0])
       t.same(fs.statSync(dirB).mode, fs.statSync(dirA).mode)
       t.same(mtime(fs.statSync(dirB)), mtime(fs.statSync(dirA)))
       t.ok(fs.statSync(dirB).isDirectory())
-      var fileB = path.join(dirB, 'test.txt')
-      var fileA = path.join(dirA, 'test.txt')
+      const fileB = path.join(dirB, 'test.txt')
+      const fileA = path.join(dirA, 'test.txt')
       t.same(fs.readFileSync(fileB, 'utf-8'), fs.readFileSync(fileA, 'utf-8'))
       t.same(fs.statSync(fileB).mode, fs.statSync(fileA).mode)
       t.same(mtime(fs.statSync(fileB)), mtime(fs.statSync(fileA)))
@@ -68,24 +68,24 @@ test('symlink', function (t) {
 
   t.plan(5)
 
-  var a = path.join(__dirname, 'fixtures', 'c')
+  const a = path.join(__dirname, 'fixtures', 'c')
 
   rimraf.sync(path.join(a, 'link'))
   fs.symlinkSync('.gitignore', path.join(a, 'link'))
 
-  var b = path.join(__dirname, 'fixtures', 'copy', 'c')
+  const b = path.join(__dirname, 'fixtures', 'copy', 'c')
 
   rimraf.sync(b)
   tar.pack(a)
     .pipe(tar.extract(b))
     .on('finish', function () {
-      var files = fs.readdirSync(b).sort()
+      const files = fs.readdirSync(b).sort()
       t.same(files.length, 2)
       t.same(files[0], '.gitignore')
       t.same(files[1], 'link')
 
-      var linkA = path.join(a, 'link')
-      var linkB = path.join(b, 'link')
+      const linkA = path.join(a, 'link')
+      const linkB = path.join(b, 'link')
 
       t.same(mtime(fs.lstatSync(linkB)), mtime(fs.lstatSync(linkA)))
       t.same(fs.readlinkSync(linkB), fs.readlinkSync(linkA))
@@ -101,24 +101,24 @@ test('follow symlinks', function (t) {
 
   t.plan(5)
 
-  var a = path.join(__dirname, 'fixtures', 'c')
+  const a = path.join(__dirname, 'fixtures', 'c')
 
   rimraf.sync(path.join(a, 'link'))
   fs.symlinkSync('.gitignore', path.join(a, 'link'))
 
-  var b = path.join(__dirname, 'fixtures', 'copy', 'c-dereference')
+  const b = path.join(__dirname, 'fixtures', 'copy', 'c-dereference')
 
   rimraf.sync(b)
   tar.pack(a, { dereference: true })
     .pipe(tar.extract(b))
     .on('finish', function () {
-      var files = fs.readdirSync(b).sort()
+      const files = fs.readdirSync(b).sort()
       t.same(files.length, 2)
       t.same(files[0], '.gitignore')
       t.same(files[1], 'link')
 
-      var file1 = path.join(b, '.gitignore')
-      var file2 = path.join(b, 'link')
+      const file1 = path.join(b, '.gitignore')
+      const file2 = path.join(b, 'link')
 
       t.same(mtime(fs.lstatSync(file1)), mtime(fs.lstatSync(file2)))
       t.same(fs.readFileSync(file1), fs.readFileSync(file2))
@@ -128,15 +128,15 @@ test('follow symlinks', function (t) {
 test('strip', function (t) {
   t.plan(2)
 
-  var a = path.join(__dirname, 'fixtures', 'b')
-  var b = path.join(__dirname, 'fixtures', 'copy', 'b-strip')
+  const a = path.join(__dirname, 'fixtures', 'b')
+  const b = path.join(__dirname, 'fixtures', 'copy', 'b-strip')
 
   rimraf.sync(b)
 
   tar.pack(a)
     .pipe(tar.extract(b, { strip: 1 }))
     .on('finish', function () {
-      var files = fs.readdirSync(b).sort()
+      const files = fs.readdirSync(b).sort()
       t.same(files.length, 1)
       t.same(files[0], 'test.txt')
     })
@@ -145,12 +145,12 @@ test('strip', function (t) {
 test('strip + map', function (t) {
   t.plan(2)
 
-  var a = path.join(__dirname, 'fixtures', 'b')
-  var b = path.join(__dirname, 'fixtures', 'copy', 'b-strip')
+  const a = path.join(__dirname, 'fixtures', 'b')
+  const b = path.join(__dirname, 'fixtures', 'copy', 'b-strip')
 
   rimraf.sync(b)
 
-  var uppercase = function (header) {
+  const uppercase = function (header) {
     header.name = header.name.toUpperCase()
     return header
   }
@@ -158,7 +158,7 @@ test('strip + map', function (t) {
   tar.pack(a)
     .pipe(tar.extract(b, { strip: 1, map: uppercase }))
     .on('finish', function () {
-      var files = fs.readdirSync(b).sort()
+      const files = fs.readdirSync(b).sort()
       t.same(files.length, 1)
       t.same(files[0], 'TEST.TXT')
     })
@@ -167,12 +167,12 @@ test('strip + map', function (t) {
 test('map + dir + permissions', function (t) {
   t.plan(win32 ? 1 : 2) // skip chmod test, it's not working like unix
 
-  var a = path.join(__dirname, 'fixtures', 'b')
-  var b = path.join(__dirname, 'fixtures', 'copy', 'a-perms')
+  const a = path.join(__dirname, 'fixtures', 'b')
+  const b = path.join(__dirname, 'fixtures', 'copy', 'a-perms')
 
   rimraf.sync(b)
 
-  var aWithMode = function (header) {
+  const aWithMode = function (header) {
     if (header.name === 'a') {
       header.mode = parseInt(700, 8)
     }
@@ -182,8 +182,8 @@ test('map + dir + permissions', function (t) {
   tar.pack(a)
     .pipe(tar.extract(b, { map: aWithMode }))
     .on('finish', function () {
-      var files = fs.readdirSync(b).sort()
-      var stat = fs.statSync(path.join(b, 'a'))
+      const files = fs.readdirSync(b).sort()
+      const stat = fs.statSync(path.join(b, 'a'))
       t.same(files.length, 1)
       if (!win32) {
         t.same(stat.mode & parseInt(777, 8), parseInt(700, 8))
@@ -194,23 +194,23 @@ test('map + dir + permissions', function (t) {
 test('specific entries', function (t) {
   t.plan(6)
 
-  var a = path.join(__dirname, 'fixtures', 'd')
-  var b = path.join(__dirname, 'fixtures', 'copy', 'd-entries')
+  const a = path.join(__dirname, 'fixtures', 'd')
+  const b = path.join(__dirname, 'fixtures', 'copy', 'd-entries')
 
-  var entries = ['file1', 'sub-files/file3', 'sub-dir']
+  const entries = ['file1', 'sub-files/file3', 'sub-dir']
 
   rimraf.sync(b)
-  tar.pack(a, { entries: entries })
+  tar.pack(a, { entries })
     .pipe(tar.extract(b))
     .on('finish', function () {
-      var files = fs.readdirSync(b)
+      const files = fs.readdirSync(b)
       t.same(files.length, 3)
       t.notSame(files.indexOf('file1'), -1)
       t.notSame(files.indexOf('sub-files'), -1)
       t.notSame(files.indexOf('sub-dir'), -1)
-      var subFiles = fs.readdirSync(path.join(b, 'sub-files'))
+      const subFiles = fs.readdirSync(path.join(b, 'sub-files'))
       t.same(subFiles, ['file3'])
-      var subDir = fs.readdirSync(path.join(b, 'sub-dir'))
+      const subDir = fs.readdirSync(path.join(b, 'sub-dir'))
       t.same(subDir, ['file5'])
     })
 })
@@ -218,9 +218,9 @@ test('specific entries', function (t) {
 test('check type while mapping header on packing', function (t) {
   t.plan(3)
 
-  var e = path.join(__dirname, 'fixtures', 'e')
+  const e = path.join(__dirname, 'fixtures', 'e')
 
-  var checkHeaderType = function (header) {
+  const checkHeaderType = function (header) {
     if (header.name.indexOf('.') === -1) t.same(header.type, header.name)
   }
 
@@ -230,26 +230,25 @@ test('check type while mapping header on packing', function (t) {
 test('finish callbacks', function (t) {
   t.plan(3)
 
-  var a = path.join(__dirname, 'fixtures', 'a')
-  var b = path.join(__dirname, 'fixtures', 'copy', 'a')
+  const a = path.join(__dirname, 'fixtures', 'a')
+  const b = path.join(__dirname, 'fixtures', 'copy', 'a')
 
   rimraf.sync(b)
 
-  var packEntries = 0
-  var extractEntries = 0
+  let packEntries = 0
+  let extractEntries = 0
 
-  var countPackEntry = function (header) { packEntries++ }
-  var countExtractEntry = function (header) { extractEntries++ }
+  const countPackEntry = function (header) { packEntries++ }
+  const countExtractEntry = function (header) { extractEntries++ }
 
-  var pack
-  var onPackFinish = function (passedPack) {
+  const onPackFinish = function (passedPack) {
     t.equal(packEntries, 2, 'All entries have been packed') // 2 entries - the file and base directory
     t.equal(passedPack, pack, 'The finish hook passes the pack')
   }
 
-  var onExtractFinish = function () { t.equal(extractEntries, 2) }
+  const onExtractFinish = function () { t.equal(extractEntries, 2) }
 
-  pack = tar.pack(a, { map: countPackEntry, finish: onPackFinish })
+  const pack = tar.pack(a, { map: countPackEntry, finish: onPackFinish })
 
   pack.pipe(tar.extract(b, { map: countExtractEntry, finish: onExtractFinish }))
     .on('finish', function () {
@@ -260,14 +259,14 @@ test('finish callbacks', function (t) {
 test('not finalizing the pack', function (t) {
   t.plan(2)
 
-  var a = path.join(__dirname, 'fixtures', 'a')
-  var b = path.join(__dirname, 'fixtures', 'b')
+  const a = path.join(__dirname, 'fixtures', 'a')
+  const b = path.join(__dirname, 'fixtures', 'b')
 
-  var out = path.join(__dirname, 'fixtures', 'copy', 'merged-packs')
+  const out = path.join(__dirname, 'fixtures', 'copy', 'merged-packs')
 
   rimraf.sync(out)
 
-  var prefixer = function (prefix) {
+  const prefixer = function (prefix) {
     return function (header) {
       header.name = path.join(prefix, header.name)
       return header
@@ -281,23 +280,23 @@ test('not finalizing the pack', function (t) {
   })
 
   function packB (pack) {
-    tar.pack(b, { pack: pack, map: prefixer('b-files') })
+    tar.pack(b, { pack, map: prefixer('b-files') })
       .pipe(tar.extract(out))
       .on('finish', assertResults)
   }
 
   function assertResults () {
-    var containers = fs.readdirSync(out)
+    const containers = fs.readdirSync(out)
     t.deepEqual(containers, ['a-files', 'b-files'])
-    var aFiles = fs.readdirSync(path.join(out, 'a-files'))
+    const aFiles = fs.readdirSync(path.join(out, 'a-files'))
     t.deepEqual(aFiles, ['hello.txt'])
   }
 })
 
 test('do not extract invalid tar', function (t) {
-  var a = path.join(__dirname, 'fixtures', 'invalid.tar')
+  const a = path.join(__dirname, 'fixtures', 'invalid.tar')
 
-  var out = path.join(__dirname, 'fixtures', 'invalid')
+  const out = path.join(__dirname, 'fixtures', 'invalid')
 
   rimraf.sync(out)
 
@@ -313,12 +312,12 @@ test('do not extract invalid tar', function (t) {
 })
 
 test('no abs hardlink targets', function (t) {
-  var out = path.join(__dirname, 'fixtures', 'invalid')
-  var outside = path.join(__dirname, 'fixtures', 'outside')
+  const out = path.join(__dirname, 'fixtures', 'invalid')
+  const outside = path.join(__dirname, 'fixtures', 'outside')
 
   rimraf.sync(out)
 
-  var s = tarStream.pack()
+  const s = tarStream.pack()
 
   fs.writeFileSync(outside, 'something')
 
