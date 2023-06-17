8fede97b9dce5d84684d40134bda0f95438d5100
Mathias Buus <mathiasbuus@gmail.com>
2023-06-17 22:38:48 +0200

    take 42

--- index.js
+++ index.js
@@ -309,8 +309,9 @@ exports.extract = function extract (cwd, opts) {
 function validate (fs, name, root, cb) {
   if (name === root) return cb(null, true)
   fs.lstat(name, function (err, st) {
-    if (err && err.code !== 'ENOENT') return cb(err)
-    if (st && st.isDirectory()) return validate(fs, path.join(name, '..'), root, cb)
+    if (err && err.code === 'ENOENT') return validate(fs, path.join(name, '..'), root, cb)
+    else if (err) return cb(err)
+    if (st.isDirectory()) return cb(null, true)
     cb(null, false)
   })
 }
